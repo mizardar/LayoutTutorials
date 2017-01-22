176b4777fc5b2ee3ed9f62946bfe59847f0c306d
mizardar <m.izardar@hotmail.com>
2017-01-22 15:46:24 +0530

    First commit

--- app/src/main/java/com/quicknexpert/layouttutorials/HelloWorldActivity.java
+++ app/src/main/java/com/quicknexpert/layouttutorials/HelloWorldActivity.java
@@ -9,5 +9,7 @@ public class HelloWorldActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_world);
+
+        //onCreate is called
     }
 }
